ee03884682956f6b240300a8340b1c07c08923c6
grayoj <Mgeraldoj07@gmail.com>
2023-05-09 18:51:29 +0100

    (feat): created a custom hook to extend context.

---
+++ src/app/hooks/useEditBanner.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { useContext } from 'react';
+import { ExitBannerContext } from '../context/ExitBannerContext';
+
+export const useEditBanner = () => useContext(ExitBannerContext);

156cf9dd073b80204e9d87f0214f28decd124d48
grayoj <Mgeraldoj07@gmail.com>
2023-05-10 13:45:19 +0100

    Feat: Created new component modules.

---
+++ src/app/modules/navigation/NavbarStyles.tsx
@@ -0,0 +1,9 @@
+// Decided to do this, to reduce JSX pollution.
+export const PopoverStyle =
+  'origin-left md:hidden transform ease-in-out transition duration-500 lg:flex-grow flex flex-col justify-center px-6 py-5 rounded lg:bg-transparent lg:static z-50 bg-white absolute right-0 top-0 w-8/12 sm:w-5/12 translate-x-0 h-screen';
+export const PopoverLink =
+  'w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base text-white bg-gray-900 hover:bg-transparent hover:text-gray-900 hover:border-gray-900 transition duration-500';
+export const NavButton =
+  'ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-900 hover:bg-transparent hover:text-gray-900 hover:border-gray-900 transition duration-500';
+export const PopoverButton =
+  'bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100';
